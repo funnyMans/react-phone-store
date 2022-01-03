de5d7feed0a3eb5519f93cfd61b4474c5607e145
Vladimir <vl.petrosyan94@gmail.com>
2022-01-03 17:36:27 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
---
+++ src/testing.js
@@ -0,0 +1,7 @@
+const testing = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default testing;
